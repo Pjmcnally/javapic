6a49791a37ebee24bf560e773eed013c07b12b2d
Patrick McNally <Patrick.J.McNally@gmail.com>
2016-02-29 21:30:40 -0800

    re-do core logic for verAll.  Ver funcs now use elements not events.

--- js/join.js
+++ js/join.js
@@ -1,60 +1,68 @@
 function load(){
+	// Add event listeners for form fields and set noValidate
 	document.getElementById("signup").noValidate = true;
-	document.getElementById("submit").addEventListener("click", verAll);
-	document.querySelector("[name=name]").addEventListener("blur", verName);
-	document.querySelector("[name=username]").addEventListener("blur", verUser);
-	document.querySelector("[name=email]").addEventListener("blur", verEmail);
+	document.getElementById("signup").addEventListener("submit", verAll);
+
+	// The verify functions work on an elem.  I use event.target to pass it in.
+	document.querySelector("[name=name]").addEventListener("blur", 
+		function() {verName(event.target);});
+	document.querySelector("[name=username]").addEventListener("blur", 
+		function() {verUser(event.target);});
+	document.querySelector("[name=email]").addEventListener("blur", 
+		function() {verEmail(event.target);});
 }
 
 // Tests if all fields have been verified.  If yes redirects if not warns user.
 function verAll(event) {
 	event.preventDefault();
-	var invalid = checkAll();
 
-	if (invalid.length === 0) {
+	verName(document.querySelector("[name=name]")); 
+	verUser(document.querySelector("[name=username]"));
+	verEmail(document.querySelector("[name=email]"));
+
+	if (checkAll()) {
 		window.location.href = "gallery.html?" + 
 			document.querySelector("[name=name]").value;
-	} else {
-		for (var i = 0; i < invalid.length; i++) {
-			setInvalid(invalid[i]);
-		}
 	}
 }
 
 // Tests for a name entry
-function verName(event) {
-	if (event.target.value){
-		setValid(event.target);
+function verName(elem) {
+	if (elem.value){
+		setValid(elem);
 	} else {
-		setInvalid(event.target);
+		setInvalid(elem);
 	}
 }
 
 // Tests for a valid username (entry that does not posses any spaces)
-function verUser(event) {
-	if (/^[^\s]+$/.test(event.target.value)){
-		setValid(event.target);
+function verUser(elem) {
+	if (/^[^\s]+$/.test(elem.value)){
+		setValid(elem);
+	} else if (/\s/.test(elem.value)){
+		setInvalid(elem, " (no spaces)");
 	} else {
-		setInvalid(event.target);
+		setInvalid(elem);
 	}
 }
 
 // Tests for a valid looking email address
-function verEmail(event) {
+function verEmail(elem) {
 	// regex to text for valid email
-	if (/^[^@\s]+@[\w\d]+.[\w\d]+$/.test(event.target.value)){
-		setValid(event.target);
+	if (/^[^@\s]+@[\w\d]+.[\w\d]+$/.test(elem.value)){
+		setValid(elem);
 	} else {
-		setInvalid(event.target);
+		setInvalid(elem);
 	}
 }
 
 // Adds invalid class to elems and modifies text as warning.
-function setInvalid(elem){
+function setInvalid(elem, message){
 	var label = elem.previousSibling.previousSibling;
+	message = message || "";
 
 	elem.className = "invalid";
-	label.innerHTML = "* " + (/\w+:/).exec(label.innerHTML)[0];
+	label.innerHTML = "* " + (/\w+:/).exec(label.innerHTML)[0] + message;
 	label.className = "invalid";
 
 	document.getElementById("warning").style.display = "block";
@@ -68,22 +76,22 @@ function setValid(elem){
 	label.className = "valid";
 	label.innerHTML = "&#10004;  "+ (/\w+:/).exec(label.innerHTML)[0];
 
-	if (checkAll().length === 0){
+	if (checkAll()){
 		document.getElementById("warning").style.display = "none";
 	}
 }
 
-// Checks all input elems.  Returns list of any elems that are invalid.
+// Checks all input elems flags.  Returns list of any elems that are invalid.
 function checkAll(){
-	var invalid = [];
 	var all = document.getElementsByTagName("input");
 
+	// -1 removes submit button from list to check.  
 	for (var i = 0; i < all.length - 1; i++){
 		if (all[i].className != "valid"){
-			invalid.push(all[i]);
+			return false;
 		}
 	}
-	return invalid;
+	return true;
 }
 
 window.onload = load;
